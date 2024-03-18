d9771997db59b6d6995e760f8792ac00032271dd
Aaron Schlotterbeck <schlotterbeck.4@wright.edu>
2024-03-18 14:36:37 +0000

    updated file

--- aws.sh
+++ aws.sh
@@ -1,1 +1,13 @@
-aws
+# Copied from pattonsgirls dotfiles
+
+if [[ -e $(ls awsdl/awscliv2.zip 2> /dev/null | head -1) ]]; then
+		echo "AWS CLI V2 installer found, running it"
+		unzip awsdl/awscliv2.zip -d awsdl/
+		./awsdl/aws/install 
+	else
+		mkdir awsdl
+		curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awsdl/awscliv2.zip"
+		unzip awsdl/awscliv2.zip -d awsdl/
+		./awsdl/aws/install
+	fi 
+
