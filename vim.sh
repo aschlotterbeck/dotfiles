2fd5be772654d6585e486054bce3491aa1bf8f60
Aaron Schlotterbeck <schlotterbeck.4@wright.edu>
2024-03-20 01:32:24 +0000

    corrected symlink issue and got plugins working

--- vim.sh
+++ vim.sh
@@ -3,9 +3,9 @@
 #!/bin/bash
 
 # Install Vundle if not already installed
-if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
+if [ ! -d "/home/$ME/.vim/bundle/Vundle.vim" ]; then
     echo "Installing Vundle..."
-    git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
+    git clone https://github.com/VundleVim/Vundle.vim.git "/home/$ME/.vim/bundle/Vundle.vim"
     echo "Vundle installed successfully"
 else
     echo "Vundle is already installed"
@@ -14,13 +14,14 @@ fi
 # Select and install a color scheme for Vim
 echo "Selecting and installing a color scheme for Vim..."
 # Replace "your_color_scheme" with the desired color scheme
-echo "colorscheme koehler" > "$HOME/.vimrc"
+echo "colorscheme koehler" > "/home/$ME/.vimrc"
 echo "Color scheme installed successfully"
 
 # Install vim plugins then add improvement plugins
 echo "Installing Plugins and then selecting a vim improvement"
-ln -sf ~/dotfiles/vimrc "$HOME/.vimrc"
+ln -sf ~/dotfiles/vimrc "/$HOME/.vimrc"
 echo "Replaced with repo vimrc"
+
 vim +PluginInstall +qall
 echo "Installed plugin list in vim"
 
